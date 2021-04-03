665812593b0e5043b1c8f326bf952764037b0d0d
Pranavvks <pranav.vks@gmail.com>
2021-04-03 14:09:08 +0530

    Made an intent to redirect user to the main page. User input , here in our app the user name is transferred to the next screen.

--- app/src/main/java/com/example/bdaycard/MainActivity.kt
+++ app/src/main/java/com/example/bdaycard/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.bdaycard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.bdaycard.databinding.ActivityMainBinding
+
+const val EXTRA_MESSAGE = "com.example.bdaycard.Message"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +15,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        val message = binding.Inputname.editableText
         binding.birthdaybutton.setOnClickListener {
-            Toast.makeText(this, "name is ${binding.Inputname.editableText}", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, BirthdayGreetingActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+                startActivity(intent)
+//            Toast.makeText(this, "name is ${binding.Inputname.editableText}", Toast.LENGTH_SHORT).show()
         }
-            setContentView(binding.root)
+
+        setContentView(binding.root)
 
 
     }
